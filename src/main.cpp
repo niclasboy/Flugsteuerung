9a0610d781477b41f12d895f5d80abca34e35d23
niclasboy <clas@online.de>
2021-11-14 20:54:28 +0100

    Test

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
 #include <Arduino.h>
+#include <Arduino_FreeRTOS.h>
+#include <avr/sleep.h>
+#include <avr/interrupt.h>
+#include <avr/io.h>
 
 void setup() {
   // put your setup code here, to run once:
