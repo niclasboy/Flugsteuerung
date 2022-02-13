fb8381f6c404d992d82a666f8c2519bc84ed2147
niclasboy <clas@online.de>
2022-02-13 15:45:59 +0100

    Update Vorort

--- src/Fulgstr/flugstr.cpp
+++ src/Fulgstr/flugstr.cpp
@@ -18,20 +18,51 @@ void applyChanges(){//Called by RX in
     pitch = map(rxDataIn[PitchChannel], 0, 255, MinPitch, MaxPitch);
     yaw = map(rxDataIn[YawChannel], 0, 255, MinYaw, MaxYaw);
     roll = map(rxDataIn[RollChannel], 0, 255, MinRoll, MaxRoll);
-    servoAngle[0] = Servo1BaseAngle;
+    servoAngle[0] = Servo1BaseAngle ;
     servoAngle[1] = Servo2BaseAngle;
-    servoAngle[2] = Servo3BaseAngle - pitch - roll; //Hinten Links Servo
-    servoAngle[3] = Servo4BaseAngle + pitch - roll; //Hinten Rechts Servo
+    servoAngle[2] = Servo3BaseAngle + pitch - roll; //Hinten Links Servo
+    servoAngle[3] = Servo4BaseAngle - pitch - roll; //Hinten Rechts Servo
     speedIn = map(rxDataIn[ThrottleChannel], 0, 255, 0, 64 - HeadroomYaw); //Eigentlich bis 
     motorSpeed[0] = speedIn + yaw;
     motorSpeed[1] = speedIn - yaw;
     motorSpeed[2] = speedIn + yaw;
     motorSpeed[3] = speedIn - yaw;    
+    //Serial.println(rxDataIn[Direction]);
+    if(rxDataIn[Direction] == 0)
+    {
+        pwm_setting[0] = constrain(motorSpeed[0],0,64);
+        pwm_setting[2] = constrain(motorSpeed[1],0,64);
+        pwm_setting[4] = constrain(motorSpeed[2],0,64);
+        pwm_setting[6] = constrain(motorSpeed[3],0,64);
+        pwm_setting[1] = 0;
+        pwm_setting[3] = 0;
+        pwm_setting[5] = 0;
+        pwm_setting[7] = 0;
+    }
+    else if(rxDataIn[Direction] == 255)
+    {
+        pwm_setting[1] = constrain(motorSpeed[0],0,64);
+        pwm_setting[3] = constrain(motorSpeed[1],0,64);
+        pwm_setting[5] = constrain(motorSpeed[2],0,64);
+        pwm_setting[7] = constrain(motorSpeed[3],0,64);
+        pwm_setting[0] = 0;
+        pwm_setting[2] = 0;
+        pwm_setting[4] = 0;
+        pwm_setting[6] = 0;
+    }
+    else
+    {
+        pwm_setting[1] = 0;
+        pwm_setting[3] = 0;
+        pwm_setting[5] = 0;
+        pwm_setting[7] = 0;
+        pwm_setting[0] = 0;
+        pwm_setting[2] = 0;
+        pwm_setting[4] = 0;
+        pwm_setting[6] = 0;
+    }
 
-    pwm_setting[0] = constrain(motorSpeed[0],0,64);
-    pwm_setting[1] = constrain(motorSpeed[1],0,64);
-    pwm_setting[2] = constrain(motorSpeed[2],0,64);
-    pwm_setting[3] = constrain(motorSpeed[3],0,64);
+
     servoAngle[0] = constrain(servoAngle[0],minServoAngle,maxServoAngle);
     servoAngle[1] = constrain(servoAngle[1],minServoAngle,maxServoAngle);
     servoAngle[2] = constrain(servoAngle[2],minServoAngle,maxServoAngle);
--- src/Fulgstr/pwm.cpp
+++ src/Fulgstr/pwm.cpp
@@ -3,20 +3,20 @@
 ISR(TIMER3_COMPA_vect) {
     static uint8_t pwm_cnt=0;
     uint8_t tmp=0;
-    
+    uint8_t tmp2=0;
 
     OCR3A += (uint16_t)T_PWM;
         
-    if (pwm_setting[0] > pwm_cnt) tmp |= (1<<7);
-    //if (pwm_setting[0] > pwm_cnt) debugCounter++;
-    if (pwm_setting[1] > pwm_cnt) tmp |= (1<<6);
-    if (pwm_setting[2] > pwm_cnt) tmp |= (1<<5);
-    if (pwm_setting[3] > pwm_cnt) tmp |= (1<<4);
-    if (pwm_setting[4] > pwm_cnt) tmp |= (1<<3);
-    if (pwm_setting[5] > pwm_cnt) tmp |= (1<<2);
-    if (pwm_setting[6] > pwm_cnt) tmp |= (1<<1);
-    if (pwm_setting[7] > pwm_cnt) tmp |= (1<<0);
+    if (pwm_setting[0] > pwm_cnt) tmp |= (1<<5);
+    if (pwm_setting[1] > pwm_cnt) tmp |= (1<<4);
+    if (pwm_setting[2] > pwm_cnt) tmp |= (1<<6);
+    if (pwm_setting[3] > pwm_cnt) tmp |= (1<<7);
+    if (pwm_setting[4] > pwm_cnt) tmp2 |= (1<<4);
+    if (pwm_setting[5] > pwm_cnt) tmp2 |= (1<<5);
+    if (pwm_setting[6] > pwm_cnt) tmp2 |= (1<<3);
+    if (pwm_setting[7] > pwm_cnt) tmp2 |= (1<<2);
     PWM_PORT = tmp;                         // PWMs aktualisieren
+    PWM_PORT2 = tmp2;                         // PWMs aktualisieren
     if (pwm_cnt==(uint8_t)(PWM_STEPS-1))
     {
         pwm_cnt=0;
@@ -29,15 +29,8 @@ ISR(TIMER3_COMPA_vect) {
 }
 
 void initMotors(){
-    pinMode(MotorEnable1, OUTPUT);
-    pinMode(MotorEnable2, OUTPUT);
-    pinMode(MotorEnable3, OUTPUT);
-    pinMode(MotorEnable4, OUTPUT);
-    digitalWrite(MotorEnable1, HIGH);
-    digitalWrite(MotorEnable2, HIGH);
-    digitalWrite(MotorEnable3, HIGH);
-    digitalWrite(MotorEnable4, HIGH);
-    PWM_DDR = 0xFF;         // Port als Ausgang    
+    PWM_DDR = 0xFF;         // Port als Ausgang  
+    PWM_DDR2 = 0xFF;         // Port als Ausgang    
     // Timer 1 OCRA1, als variablem Timer nutzen
     TCCR3B = 1;             // Timer läuft mit vollem Systemtakt
     TIMSK3 = (1<<OCIE3A);   // Interrupt freischalten
